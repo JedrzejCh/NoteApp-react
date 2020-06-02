b4c1b33d3002531a33875060e9a98403a0d328d1
Jędrzej  Choniawko <jchoniawko@gmail.com>
2020-06-02 17:05:29 +0200

    Updates

---
+++ precache-manifest.3b1c2033d503a976da28c1c6de85c9de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6644ef17382cf8fe4050df7fd8916908",
+    "url": "/NoteApp-react/index.html"
+  },
+  {
+    "revision": "d0f2a9279991ede5ce18",
+    "url": "/NoteApp-react/static/css/main.1690c0b3.chunk.css"
+  },
+  {
+    "revision": "ab50f896e79f39993453",
+    "url": "/NoteApp-react/static/js/2.f7d11d1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NoteApp-react/static/js/2.f7d11d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f2a9279991ede5ce18",
+    "url": "/NoteApp-react/static/js/main.74e431d3.chunk.js"
+  },
+  {
+    "revision": "e955a3612fdcf032305c",
+    "url": "/NoteApp-react/static/js/runtime-main.14c40f8f.js"
+  }
+]);
